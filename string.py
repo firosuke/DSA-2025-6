e798d242cf01cc926a8861a1a6a722201f68e7b9
Faris Abou-Saleh <superfaris@googlemail.com>
2025-10-15 20:54:01 +0100

    Create string.py

---
+++ string.py
@@ -0,0 +1,18 @@
+###############################################################################################
+# String wrapper(!) to prevent you from doing fancy Python stuff that I don't want you to use #
+###############################################################################################
+
+def String:
+  _s: str
+  def __init__(self, s: str):
+    self._s = s
+  def read(self, i) -> str:
+    return self._s[i]
+  def __str__(self) -> str: # The irony of this method
+    return self._s
+  def __repr__(self) -> str:
+    return str(self)
+  def __eq__(self, other) -> bool:
+    if not isinstance(other, String):
+      return False
+    return other._s == this._s
