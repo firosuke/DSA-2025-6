5386bb902a86f57fb7a308ab1d68a6ef193ca1d4
Faris Abou-Saleh <superfaris@googlemail.com>
2025-10-16 04:16:44 +0100

    Update mat.py

--- mat.py
+++ mat.py
@@ -1,3 +1,5 @@
+# FREEZE CODE BEGIN
+
 from lst import Lst
 
 from typing import TypeVar, Generic
@@ -49,4 +51,39 @@ class Mat(Generic[T]):
   def __repr__(self) -> str:
     return str(self)
 
+def generate(S: type, **params) -> Mat: # Class method
+  rows = randint(2, 4)
+  cols = randint(2, 4)
+  data = []
+  if S == int: # Special case: Somewhat-smoothly varying integers
+    p = "positive_only" in params and params["positive_only"]
+    data.append([randint(3, 9)])
+    for _ in range(1, cols):
+      v = data[0][-1] + 0 * int(data[0][-1] * (0.6 + 0.4 * random())) + randint(-3, 3)
+      v = abs(v) if p else v
+      data[0].append(v)
+    for _ in range(1, rows):
+      v = data[-1][0] + 0 * int(data[-1][0] * (0.6 + 0.4 * random())) + randint(-3, 3)
+      v = abs(v) if p else v
+      data.append([v])
+    for i in range(1, rows):
+      for j in range(1, cols):
+        v = int((c := random()) * data[i][-1] + (1 - c) * data[i - 1][j]) + randint(-3, 3)
+        v = abs(v) if p else v
+        data[i].append(v)
+  else:
+    data = []
+    for _ in range(rows):
+      row = []
+      for _ in range(cols):
+        row.append(S.generate(**params))
+      data.append(row)
+  M = Mat(rows, cols, 0)
+  for i in range(rows):
+    for j in range(cols):
+      M._lofl.read(i).write(j, data[i][j])
+  return M
+
   # TODO: __eq__, __hash__ ?
+
+# FREEZE CODE END
