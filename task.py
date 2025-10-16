3d9f046d4a49d4660a1c650df8a2863984928b63
Faris Abou-Saleh <superfaris@googlemail.com>
2025-10-16 03:12:28 +0100

    Update task.py

--- task.py
+++ task.py
@@ -19,23 +19,23 @@ class Task:
   # Constructor
   def __init__(self, name, is_high_priority, duration):
     self._name = name
-    self.is_high_priority = is_high_priority
+    self._is_high_priority = is_high_priority
     self._remaining_time = duration
 
   # Public methods
   def run(self, period: int) -> None:
     run_duration = min(period, self._remaining_time)
-    print("Running task", self.name, "for", run_duration, "clock cycles")
+    print("Running task", self._name, "for", run_duration, "clock cycles")
     self._remaining_time = max(0, self._remaining_time - period)
     if self._remaining_time == 0:
-      print("Task", self.name, "completed")
+      print("Task", self._name, "completed")
 
   def is_complete(self) -> bool:
     return self._remaining_time == 0  
 
   # Display methods
   def __str__(self) -> str:
-    return f"Task({self.name}, {self.is_high_priority}, {self._remaining_time})"
+    return f"Task({self._name}, {self._is_high_priority}, {self._remaining_time})"
 
   def __repr__(self) -> str:
     return str(self)
