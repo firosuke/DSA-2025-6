edfac4a5b07fd68660ea51c3f8d158582b31a7db
Faris Abou-Saleh <superfaris@googlemail.com>
2025-10-16 02:29:07 +0100

    Update string.py

--- string.py
+++ string.py
@@ -4,7 +4,7 @@ from random import randint, random, sample
 # String wrapper(!) to prevent you from doing fancy Python stuff that I don't want you to use #
 ###############################################################################################
 
-def String:
+class String:
   _s: str
   def __init__(self, s: str):
     self._s = s
@@ -13,7 +13,7 @@ def String:
   def size(self) -> int:
     return len(self._s)
   def find(self, c) -> int:
-    return _s.find(c)
+    return self._s.find(c)
   def __str__(self) -> str: # The irony of this method
     return f"String({self._s})"
   def __repr__(self) -> str:
@@ -32,12 +32,15 @@ def String:
       i = randint(0, len(s))
       r = random() 
       if r > 0.4:
+        # Insert matched bracket pair
         b = randint(0, len(o) - 1) 
         s.insert(i, c[b])
         s.insert(i, o[b])
       elif r > 0.1:
+        # Insert random letter
         s.insert(i, sample("abcdefgh", 1)[0])
       else:
+        # Insert unmatched bracket
         b = randint(0, len(o) - 1)
         s.insert(i, c[b] if random() > 0.5 else o[b])
     return "".join(s)    
