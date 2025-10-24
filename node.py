19c24b73a9ccfac99fbb320b2fbb77836bf86c9c
Faris Abou-Saleh <superfaris@googlemail.com>
2025-10-24 05:18:02 +0100

    Create node.py

---
+++ node.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+from typing import Generic, Optional, TypeVar
+
+T = TypeVar('T')
+
+####################################
+# Node, to be used in binary trees #
+####################################
+class Node(Generic[T]):
+  # Type annotations
+  _value: T
+  _left: Optional[Link[T]]
+  _right: Optional[Link[T]]
+
+  def __init__(self, value: T, left: Optional[Node[T]], right: Optional[Node[T]]):
+    self._value = value
+    self._left = left
+    self._right = right
+
+  def set_value(self, value: T) -> None:
+    self._value = value
+
+  def get_value(self) -> T:
+    return self._value
+
+  def set_left(self, left: Node[T]) -> None:
+    self._left = left
+
+  def get_left(self) -> Optional[Node[T]]:
+    return self._left
+
+  def set_right(self, right: Node[T]) -> None:
+    self._right = right
+
+  def get_right(self) -> Optional[Node[T]]:
+    return self._right
+  
+  def __str__(self):
+    v = self._value
+    v = f'"{v}"' if isinstance(v, str)
+    return f"Node({v}, {str(self._left)}, {str(self._right)})"
+
+  def __repr__(self):
+    return str(self)
