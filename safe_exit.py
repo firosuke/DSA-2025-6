655be60fe74769888f14770e6b1d3b8ffd2dfe02
Faris Abou-Saleh <superfaris@googlemail.com>
2026-01-07 11:44:44 +0000

    Update safe_exit.py

--- safe_exit.py
+++ safe_exit.py
@@ -2,11 +2,10 @@
 try:
   from sys import argv
   import subprocess
-  keywords = {"exercise", "example", "solution", "coursework", "practise", "test", "revision"}
+  keywords = {"coding", "exercise", "example", "solution", "coursework", "practise", "test", "revision"}
   if not any(x in argv[-1] for x in keywords):
-    from colortext import printc
-    printc("To carry out this action, please click on a suitable Python file (e.g. examples, exercises, solutions, or coursework) to be the target, and then try again.", "light-red")
-    printc("Press Enter to close this tab", "light-green")
+    print("To carry out this action, please click on a suitable Python file (e.g. examples, exercises, solutions, or coursework) to be the target, and then try again.")
+    print("Press Enter to close this tab")
   else:
     subprocess.run(argv[1:])
   input()
